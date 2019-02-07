df3c153d789562f7ba774d9a6c3965515a169999
will-o-the-wisp <kli18@stuy.edu>
2019-02-07 09:31:17 -0500

    added empty functions

---
+++ QueenBoard.java
@@ -0,0 +1,70 @@
+public class QueenBoard{
+  private int[][]board;
+  public static void main(String[] args){
+    
+  }
+  public QueenBoard(int size){
+
+  }
+  private boolean addQueen(int r, int c){
+
+  }
+  private boolean removeQueen(int r, int c){
+
+  }
+  /**
+  *@return The output string formatted as follows:
+  *All numbers that represent queens are replaced with 'Q'
+  *all others are displayed as underscores '_'
+  *There are spaces between each symbol:
+  *"""_ _ Q _
+  *Q _ _ _
+  *_ _ _ Q
+  *_ Q _ _"""
+  *(pythonic string notation for clarity,
+  *excludes the character up to the *)
+  */
+  public String toString(){
+
+  }
+  private int[][]board;
+  public QueenBoard(int size){
+
+  }
+  private boolean addQueen(int r, int c){
+
+  }
+  private boolean removeQueen(int r, int c){
+
+  }
+  /**
+  *@return The output string formatted as follows:
+  *All numbers that represent queens are replaced with 'Q'
+  *all others are displayed as underscores '_'
+  *There are spaces between each symbol:
+  *"""_ _ Q _
+  *Q _ _ _
+  *_ _ _ Q
+  *_ Q _ _"""
+  *(pythonic string notation for clarity,
+  *excludes the character up to the *)
+  */
+  public String toS
+
+  /**
+  *@return false when the board is not solveable and leaves the board filled with zeros;
+  *        true when the board is solveable, and leaves the board in a solved state
+  *@throws IllegalStateException when the board starts with any non-zero value
+  */
+  public boolean solve(){
+
+  }
+
+  /**
+  *@return the number of solutions found, and leaves the board filled with only 0's
+  *@throws IllegalStateException when the board starts with any non-zero value
+  */
+  public int countSolutions(){
+
+  }
+}
